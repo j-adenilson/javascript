adc3427bd50cbb4b4f48ab213bac418f8671552c
j-adenilson <j.adenilson@outlook.com.br>
2025-07-02 23:16:50 -0300

    tipos de dados

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,14 @@ let idade = 32;
 
 console.log("meu nome é: " + " " + nome);
 console.log(idade + " " + "anos");
+
+let nome2 = "José"; // tipo string
+let idade2 = 32 // tipo number, utilizar(.) para separar numeros
+let logado = true; // tipo boolean - representa (sim ou não) - true or false
+let lista = ["água", "terra", "ar", "fogo"]; // tipo array (uma lista)
+let objeto = {
+    nome: "José",
+    idade: 32
+    }; // tipo objeto
+
+console.log(typeof objeto)
